e9d180a386fd775569232bf06af47b69e8815a1a
chikirao <ivankonanch@gmail.com>
2024-11-19 00:12:14 +0300

    flake fixed

--- calculate.py
+++ calculate.py
@@ -1,40 +1,47 @@
 import circle
 import square
 
-figs = ['circle', 'square']
-funcs = ['perimeter', 'area']
+figs = ["circle", "square"]
+funcs = ["perimeter", "area"]
+
 
 def calc(fig, func, size):
-    '''
+    """
     Выполняет расчет для выбранной фигуры и функции.
-    
+
     Аргументы:
     fig -- строка, обозначающая фигуру ('circle' или 'square')
     func -- строка, обозначающая функцию ('perimeter' или 'area')
     size -- список с размерами, необходимыми для расчета
-    
-    Возвращает результат расчета.
-    '''
-    if fig not in figs:
-        raise ValueError(f"Unknown figure: {fig}. Valid options are {figs}.")
-    if func not in funcs:
-        raise ValueError(f"Unknown function: {func}. Valid options are {funcs}.")
 
-    if fig == 'circle':
-        if func == 'area':
+    Возвращает результат расчета.
+    """
+    if fig not in figs:
+        raise ValueError(
+            f"Unknown figure: {fig}. Valid options are {figs}."
+        )
+    if func not in funcs:
+        raise ValueError(
+            f"Unknown function: {func}. Valid options are {funcs}."
+        )
+
+    if fig == "circle":
+        if func == "area":
             return circle.area(*size)
-        elif func == 'perimeter':
+        if func == "perimeter":
             return circle.perimeter(*size)
-    elif fig == 'square':
-        if func == 'area':
+    if fig == "square":
+        if func == "area":
             return square.area(*size)
-        elif func == 'perimeter':
+        if func == "perimeter":
             return square.perimeter(*size)
+
 
 if __name__ == "__main__":
     fig = input(f"Enter figure name (available: {figs}):\n").strip()
     func = input(f"Enter function name (available: {funcs}):\n").strip()
-    size = list(map(float, input("Enter size(s), separated by spaces:\n").split()))
+    size_inp_msg = "Enter size(s), separated by spaces:\n"
+    size = list(map(float, input(size_inp_msg).split()))
 
     try:
         result = calc(fig, func, size)
